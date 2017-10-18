55e1706ad9987405c8113acaf43ffb2d04a7ce65
Matthew Renn <rennmatthewp@gmail.com>
2017-10-18 11:02:30 -0600

    hooks up tests to sorting functions

---
+++ test/bubble-test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const bubbleSort = require('../lib/bubble.js');
+
+describe('bubbleSort', () => {
+
+  it('should be a function', () => {
+    assert.isFunction(bubbleSort);
+  });
+  
+});
---
+++ test/quick-test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const quickSort = require('../lib/quick.js');
+
+describe('quickSort', () => {
+
+  it('should be a function', () => {
+    assert.isFunction(quickSort);
+  });
+  
+});
